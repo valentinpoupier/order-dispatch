5ce77dacd9aa05e0c2306ac8e5d806a6e0aee818
Julie Vanderbyse <vanderbysej@gmail.com>
2023-04-21 13:44:44 +0200

    first commit

---
+++ src/main/java/technifutur/java/order_dispatch/OrderDispatchApplication.java
@@ -0,0 +1,13 @@
+package technifutur.java.order_dispatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderDispatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderDispatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/technifutur/java/order_dispatch/OrderDispatchApplicationTests.java
@@ -0,0 +1,13 @@
+package technifutur.java.order_dispatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderDispatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
