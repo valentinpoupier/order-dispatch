a9b535f320d09e060f55583c2c364e7d59afee46
Timé Prégardien <time.pregardien@gmail.com>
2023-04-21 15:40:26 +0200

    service&controller

---
+++ src/main/java/technifutur/java/order_dispatch/controller/OrderController.java
@@ -0,0 +1,14 @@
+package technifutur.java.order_dispatch.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+
+
+
+}
---
+++ src/main/java/technifutur/java/order_dispatch/service/impl/OrderServiceImpl.java
@@ -0,0 +1,7 @@
+package technifutur.java.order_dispatch.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl {
+}
